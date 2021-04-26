08036ede7ddc4c43131ef54daa7fa9fa3d5e541b
fenibo <fenibz@gmail.com>
2021-04-26 23:54:52 +0100

    all

---
+++ includes/footer.php
@@ -0,0 +1,61 @@
+  <footer class="ftco-footer ftco-section">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">About</h2>
+              <p>I am an ever passionate software engineer with a sole drive of offering solutions to human problems via technology deployment</p>
+              <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
+                <li class="ftco-animate"><a href="https://twitter.com/Superfenic"><span class="icon-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="https://web.facebook.com/fenibo.fubara"><span class="icon-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="https://www.linkedin.com/in/fenibo-fubara-b1880b1a5/"><span class="icon-linkedin"></span></a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4 ml-md-4">
+              <h2 class="ftco-heading-2">Links</h2>
+              <ul class="list-unstyled">
+                <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Home</a></li>
+                <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>About</a></li>
+                <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Services</a></li>
+                <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Projects</a></li>
+                <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Contact</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+             <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Services</h2>
+              <ul class="list-unstyled">
+                <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Web Development</a></li>
+                <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Mobile Development</a></li>
+                <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Public Speaking</a></li>
+                <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>First Principle Thinking</a></li>
+          
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+            	<h2 class="ftco-heading-2">Have a Questions?</h2>
+            	<div class="block-23 mb-3">
+	              <ul>
+	                <li><span class="icon icon-map-marker"></span><span class="text">Port Harcourt, Nigeria</span></li>
+	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2347038850669</span></a></li>
+	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">fenibz@gmail.com</span></a></li>
+	              </ul>
+	            </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center">
+
+            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This site is designed<i class="icon-heart color-danger" aria-hidden="true"></i> by <a href="https://supermanic.com" target="_blank">SuperManic</a>
+  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+          </div>
+        </div>
+      </div>
+    </footer>
